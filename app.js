3656e13313f8a2dfb974ebd5b4984a0aa3ad8787
jessehancock <jessehancock1@gmail.com>
2016-12-13 12:06:21 -0700

    put project steps in file

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+//NEED A SIMPLE WAY to LOOK AT BADGE COUNT AND JAVASCRIPT POINTS ON TEAMTREEHOUSE
+//USE NODE.JS TO CONNECT TO TREEHOUSES API
+
+function printMessage(username, badgeCount, points){
+  const message = username + " has " + badgeCount + " total badge(s) and " + points + " in JavaScript"
+  console.log(message);
+}
+
+printMessage('jesse', 100, 1000);
+
+
+//CONNECT TO THE API URL (http://teamtreehouse.com/jessehancock2.json)
+//Read the data
+//Parse the data
+//Print the data
