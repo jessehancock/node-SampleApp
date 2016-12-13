4fca598fff7375dc23f92c51671ca2e97ee65044
jessehancock <jessehancock1@gmail.com>
2016-12-13 13:16:51 -0700

    testing get api call

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 //NEED A SIMPLE WAY to LOOK AT BADGE COUNT AND JAVASCRIPT POINTS ON TEAMTREEHOUSE
 //USE NODE.JS TO CONNECT TO TREEHOUSES API
+const https = require("https");
+
+const username = "jessehancock2";
 
 function printMessage(username, badgeCount, points){
   const message = username + " has " + badgeCount + " total badge(s) and " + points + " in JavaScript"
   console.log(message);
 }
 
-printMessage('jesse', 100, 1000);
+const request = https.get("https://teamtreehouse.com/" + username + ".json", function(response){
+  console.log(response.statusCode);
+})
 
 
 //CONNECT TO THE API URL (http://teamtreehouse.com/jessehancock2.json)
