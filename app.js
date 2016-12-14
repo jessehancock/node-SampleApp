7cf67b0abb770f63b0fc4c2f7d7c40fd1419c196
jessehancock <jessehancock1@gmail.com>
2016-12-13 17:05:19 -0700

    added process.argv to allow typing in thing in the terminal

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const profile = require("./profile.js");
 
-profile.get("jessehancock2");
-profile.get("chalkers");
+// let users = ["jessehancock2", "davemcfarland", "chalkers"]; //if you dont use process
+
+//process.argv lets returns an array with users after you type it in the terminal ex. node app.js jessehancock2
+let users = process.argv.slice(2);
+
+users.forEach(profile.get);
